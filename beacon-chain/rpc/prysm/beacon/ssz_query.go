add593acdb541c7e5a5bf8fc0d094fe147625d84
Jun Song <syjn99@gmail.com>
2025-07-18 18:38:52 +0900

    feat: construct basic information for Beacon API response & define structs

--- beacon-chain/rpc/prysm/beacon/ssz_query.go
+++ beacon-chain/rpc/prysm/beacon/ssz_query.go
@@ -1,9 +1,95 @@
 package beacon
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+
+	"github.com/OffchainLabs/prysm/v6/api/server/structs"
+	"github.com/OffchainLabs/prysm/v6/beacon-chain/rpc/eth/helpers"
+	"github.com/OffchainLabs/prysm/v6/beacon-chain/rpc/eth/shared"
+	"github.com/OffchainLabs/prysm/v6/beacon-chain/rpc/lookup"
+	"github.com/OffchainLabs/prysm/v6/monitoring/tracing/trace"
+	"github.com/OffchainLabs/prysm/v6/network/httputil"
+	"github.com/OffchainLabs/prysm/v6/runtime/version"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 func (s *Server) QuerySSZ(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	ctx, span := trace.StartSpan(r.Context(), "beacon.QuerySSZ")
+	defer span.End()
+
+	stateID := r.PathValue("state_id")
+	if stateID == "" {
+		httputil.HandleError(w, "state_id is required in URL params", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch state root for the given state ID.
+	stateRoot, err := s.Stater.StateRoot(ctx, []byte(stateID))
+	if err != nil {
+		var rootNotFoundErr *lookup.StateRootNotFoundError
+		if errors.As(err, &rootNotFoundErr) {
+			httputil.HandleError(w, "State root not found: "+rootNotFoundErr.Error(), http.StatusNotFound)
+			return
+		}
+		httputil.HandleError(w, "Could not get state root: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Fetch state from given state ID.
+	st, err := s.Stater.State(ctx, []byte(stateID))
+	if err != nil {
+		shared.WriteStateFetchError(w, err)
+		return
+	}
+
+	// Gather `execution_optimistic` and `finalized` flags.
+	isOptimistic, err := helpers.IsOptimistic(ctx, []byte(stateID), s.OptimisticModeFetcher, s.Stater, s.ChainInfoFetcher, s.BeaconDB)
+	if err != nil {
+		httputil.HandleError(w, "Could not check optimistic status: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	blockRoot, err := st.LatestBlockHeader().HashTreeRoot()
+	if err != nil {
+		httputil.HandleError(w, "Could not calculate root of latest block header: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	isFinalized := s.FinalizationFetcher.IsFinalized(ctx, blockRoot)
+
+	// Parse request body.
+	var req structs.QuerySSZRequest
+	err = json.NewDecoder(r.Body).Decode(&req)
+	switch {
+	case errors.Is(err, io.EOF):
+		httputil.HandleError(w, "No data submitted", http.StatusBadRequest)
+		return
+	case err != nil:
+		httputil.HandleError(w, "Could not decode request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(req.Query) == 0 {
+		httputil.HandleError(w, "No query submitted", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: Implement the actual SSZ query logic.
+	// For now, as we can't build proofs,
+	// just assume `req.IncludeProof` and `req.Multiproof` are both false.
+
+	// End of the TODO section.
+
+	querySSZResponse := &structs.QuerySSZResponse{
+		Version:             version.String(st.Version()),
+		ExecutionOptimistic: isOptimistic,
+		Finalized:           isFinalized,
+		Data: &structs.QuerySSZData{
+			Result: []*structs.QueryResultItem{}, // Placeholder.
+			Root:   hexutil.Encode(stateRoot),
+		},
+	}
+
+	httputil.WriteJson(w, querySSZResponse)
 }
