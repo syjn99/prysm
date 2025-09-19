02cba58aa69d5eca8abac51ecbe92314257946c4
Jun Song <syjn99@gmail.com>
2025-09-20 00:20:03 +0900

    Add (passing) BeaconState test

--- encoding/ssz/query/query_test.go
+++ encoding/ssz/query/query_test.go
@@ -6,8 +6,10 @@ import (
 
 	"github.com/OffchainLabs/prysm/v6/encoding/ssz/query"
 	"github.com/OffchainLabs/prysm/v6/encoding/ssz/query/testutil"
+	ethpb "github.com/OffchainLabs/prysm/v6/proto/prysm/v1alpha1"
 	sszquerypb "github.com/OffchainLabs/prysm/v6/proto/ssz_query"
 	"github.com/OffchainLabs/prysm/v6/testing/require"
+	"github.com/OffchainLabs/prysm/v6/testing/util"
 	"github.com/prysmaticlabs/go-bitfield"
 )
 
@@ -211,6 +213,7 @@ func TestRoundTripSszInfo(t *testing.T) {
 	specs := []testutil.TestSpec{
 		getFixedTestContainerSpec(),
 		getVariableTestContainerSpec(),
+		getBeaconStateElectraSpec(),
 	}
 
 	for _, spec := range specs {
@@ -457,4 +460,372 @@ func getVariableTestContainerSpec() testutil.TestSpec {
 			},
 		},
 	}
+}
+
+func getBeaconStateElectraSpec() testutil.TestSpec {
+	// Create a BeaconStateElectra with test data
+	beaconState, err := util.NewBeaconStateElectra()
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the underlying protobuf state
+	pbState := beaconState.ToProtoUnsafe().(*ethpb.BeaconStateElectra)
+
+	// Set some test values
+	pbState.Slot = 54321
+	pbState.GenesisTime = 1609459200
+	pbState.GenesisValidatorsRoot = generateUniqueBytes(32, 0)
+	pbState.Eth1DepositIndex = 999
+	pbState.Fork.Epoch = 12345
+	pbState.Fork.CurrentVersion = []byte{1, 2, 3, 4}
+	pbState.Fork.PreviousVersion = []byte{5, 6, 7, 8}
+	pbState.LatestBlockHeader.Slot = 54320
+	pbState.LatestBlockHeader.ProposerIndex = 100
+	pbState.LatestBlockHeader.ParentRoot = generateUniqueBytes(32, 32)
+	pbState.LatestBlockHeader.StateRoot = generateUniqueBytes(32, 64)
+	pbState.LatestBlockHeader.BodyRoot = generateUniqueBytes(32, 96)
+	pbState.Eth1Data.DepositRoot = generateUniqueBytes(32, 128)
+	pbState.Eth1Data.DepositCount = 500
+	pbState.Eth1Data.BlockHash = generateUniqueBytes(32, 160)
+	pbState.Balances = []uint64{32000000000, 31000000000, 33000000000, 32500000000, 31500000000}
+	pbState.JustificationBits = bitfield.Bitvector4{0x0F}
+
+	// Add validators with test data
+	pbState.Validators = []*ethpb.Validator{
+		{
+			PublicKey:                  generateUniqueBytes(48, 192),
+			WithdrawalCredentials:      generateUniqueBytes(32, 240),
+			EffectiveBalance:           32000000000,
+			Slashed:                    false,
+			ActivationEligibilityEpoch: 0,
+			ActivationEpoch:            0,
+			ExitEpoch:                  18446744073709551615,
+			WithdrawableEpoch:          18446744073709551615,
+		},
+		{
+			PublicKey:                  generateUniqueBytes(48, 272),
+			WithdrawalCredentials:      generateUniqueBytes(32, 320),
+			EffectiveBalance:           31000000000,
+			Slashed:                    true,
+			ActivationEligibilityEpoch: 1,
+			ActivationEpoch:            2,
+			ExitEpoch:                  1000,
+			WithdrawableEpoch:          1100,
+		},
+		{
+			PublicKey:                  generateUniqueBytes(48, 352),
+			WithdrawalCredentials:      generateUniqueBytes(32, 400),
+			EffectiveBalance:           33000000000,
+			Slashed:                    false,
+			ActivationEligibilityEpoch: 10,
+			ActivationEpoch:            15,
+			ExitEpoch:                  18446744073709551615,
+			WithdrawableEpoch:          18446744073709551615,
+		},
+	}
+
+	// Add more populated fields
+	pbState.Slashings[0] = 1000000000
+	pbState.Slashings[100] = 2000000000
+	pbState.Slashings[1000] = 3000000000
+
+	// Add some eth1 data votes
+	pbState.Eth1DataVotes = []*ethpb.Eth1Data{
+		{
+			DepositRoot:  generateUniqueBytes(32, 432),
+			DepositCount: 100,
+			BlockHash:    generateUniqueBytes(32, 464),
+		},
+		{
+			DepositRoot:  generateUniqueBytes(32, 496),
+			DepositCount: 200,
+			BlockHash:    generateUniqueBytes(32, 528),
+		},
+	}
+
+	// Set some participation flags
+	pbState.PreviousEpochParticipation = []byte{0x07, 0x05, 0x03}
+	pbState.CurrentEpochParticipation = []byte{0x07, 0x07, 0x01, 0x00, 0x06}
+
+	// Set inactivity scores
+	pbState.InactivityScores = []uint64{0, 0, 10, 5, 100}
+
+	// Set checkpoint fields
+	pbState.PreviousJustifiedCheckpoint = &ethpb.Checkpoint{
+		Epoch: 100,
+		Root:  generateUniqueBytes(32, 1040),
+	}
+	pbState.CurrentJustifiedCheckpoint = &ethpb.Checkpoint{
+		Epoch: 101,
+		Root:  generateUniqueBytes(32, 1072),
+	}
+	pbState.FinalizedCheckpoint = &ethpb.Checkpoint{
+		Epoch: 99,
+		Root:  generateUniqueBytes(32, 1104),
+	}
+
+	// Set next withdrawal indices
+	pbState.NextWithdrawalIndex = 42
+	pbState.NextWithdrawalValidatorIndex = 123
+
+	// Set Electra-specific fields
+	pbState.DepositRequestsStartIndex = 5000
+	pbState.DepositBalanceToConsume = 64000000000
+	pbState.ExitBalanceToConsume = 32000000000
+	pbState.EarliestExitEpoch = 100
+	pbState.ConsolidationBalanceToConsume = 96000000000
+	pbState.EarliestConsolidationEpoch = 200
+
+	// Add historical summaries
+	pbState.HistoricalSummaries = []*ethpb.HistoricalSummary{
+		{
+			BlockSummaryRoot: generateUniqueBytes(32, 560),
+			StateSummaryRoot: generateUniqueBytes(32, 592),
+		},
+		{
+			BlockSummaryRoot: generateUniqueBytes(32, 624),
+			StateSummaryRoot: generateUniqueBytes(32, 656),
+		},
+	}
+
+	// Add pending deposits
+	pbState.PendingDeposits = []*ethpb.PendingDeposit{
+		{
+			PublicKey:             generateUniqueBytes(48, 688),
+			WithdrawalCredentials: generateUniqueBytes(32, 736),
+			Amount:                32000000000,
+			Signature:             generateUniqueBytes(96, 768),
+			Slot:                  10000,
+		},
+		{
+			PublicKey:             generateUniqueBytes(48, 864),
+			WithdrawalCredentials: generateUniqueBytes(32, 912),
+			Amount:                16000000000,
+			Signature:             generateUniqueBytes(96, 944),
+			Slot:                  10100,
+		},
+	}
+
+	// Add pending partial withdrawals
+	pbState.PendingPartialWithdrawals = []*ethpb.PendingPartialWithdrawal{
+		{
+			Index:             0,
+			Amount:            1000000000,
+			WithdrawableEpoch: 500,
+		},
+		{
+			Index:             1,
+			Amount:            2000000000,
+			WithdrawableEpoch: 510,
+		},
+		{
+			Index:             2,
+			Amount:            1500000000,
+			WithdrawableEpoch: 520,
+		},
+	}
+
+	// Add pending consolidations
+	pbState.PendingConsolidations = []*ethpb.PendingConsolidation{
+		{
+			SourceIndex: 0,
+			TargetIndex: 1,
+		},
+	}
+
+	return testutil.TestSpec{
+		Name:     "BeaconStateElectra",
+		Type:     ethpb.BeaconStateElectra{},
+		Instance: pbState,
+		PathTests: []testutil.PathTest{
+			// Test basic fields
+			{
+				Path:     ".slot",
+				Expected: pbState.Slot,
+			},
+			{
+				Path:     ".genesis_time",
+				Expected: pbState.GenesisTime,
+			},
+			{
+				Path:     ".genesis_validators_root",
+				Expected: pbState.GenesisValidatorsRoot,
+			},
+			// Test fork data
+			{
+				Path:     ".fork.epoch",
+				Expected: pbState.Fork.Epoch,
+			},
+			{
+				Path:     ".fork.current_version",
+				Expected: pbState.Fork.CurrentVersion,
+			},
+			{
+				Path:     ".fork.previous_version",
+				Expected: pbState.Fork.PreviousVersion,
+			},
+			// Test latest block header
+			{
+				Path:     ".latest_block_header.slot",
+				Expected: pbState.LatestBlockHeader.Slot,
+			},
+			{
+				Path:     ".latest_block_header.proposer_index",
+				Expected: pbState.LatestBlockHeader.ProposerIndex,
+			},
+			{
+				Path:     ".latest_block_header.parent_root",
+				Expected: pbState.LatestBlockHeader.ParentRoot,
+			},
+			{
+				Path:     ".latest_block_header.state_root",
+				Expected: pbState.LatestBlockHeader.StateRoot,
+			},
+			{
+				Path:     ".latest_block_header.body_root",
+				Expected: pbState.LatestBlockHeader.BodyRoot,
+			},
+			// Test eth1 data
+			{
+				Path:     ".eth1_data.deposit_root",
+				Expected: pbState.Eth1Data.DepositRoot,
+			},
+			{
+				Path:     ".eth1_data.deposit_count",
+				Expected: pbState.Eth1Data.DepositCount,
+			},
+			{
+				Path:     ".eth1_data.block_hash",
+				Expected: pbState.Eth1Data.BlockHash,
+			},
+			{
+				Path:     ".eth1_deposit_index",
+				Expected: pbState.Eth1DepositIndex,
+			},
+			// Test validators - entire list
+			{
+				Path:     ".validators",
+				Expected: pbState.Validators,
+			},
+			// Test balances - entire list
+			{
+				Path:     ".balances",
+				Expected: pbState.Balances,
+			},
+			// Test participation
+			{
+				Path:     ".previous_epoch_participation",
+				Expected: pbState.PreviousEpochParticipation,
+			},
+			{
+				Path:     ".current_epoch_participation",
+				Expected: pbState.CurrentEpochParticipation,
+			},
+			// Test eth1 data votes - entire list
+			{
+				Path:     ".eth1_data_votes",
+				Expected: pbState.Eth1DataVotes,
+			},
+			// Test inactivity scores - entire list
+			{
+				Path:     ".inactivity_scores",
+				Expected: pbState.InactivityScores,
+			},
+			// Test justification bits
+			{
+				Path:     ".justification_bits",
+				Expected: pbState.JustificationBits,
+			},
+			// Test checkpoint fields
+			{
+				Path:     ".previous_justified_checkpoint.epoch",
+				Expected: pbState.PreviousJustifiedCheckpoint.Epoch,
+			},
+			{
+				Path:     ".previous_justified_checkpoint.root",
+				Expected: pbState.PreviousJustifiedCheckpoint.Root,
+			},
+			{
+				Path:     ".current_justified_checkpoint.epoch",
+				Expected: pbState.CurrentJustifiedCheckpoint.Epoch,
+			},
+			{
+				Path:     ".current_justified_checkpoint.root",
+				Expected: pbState.CurrentJustifiedCheckpoint.Root,
+			},
+			{
+				Path:     ".finalized_checkpoint.epoch",
+				Expected: pbState.FinalizedCheckpoint.Epoch,
+			},
+			{
+				Path:     ".finalized_checkpoint.root",
+				Expected: pbState.FinalizedCheckpoint.Root,
+			},
+			// Test withdrawal fields
+			{
+				Path:     ".next_withdrawal_index",
+				Expected: pbState.NextWithdrawalIndex,
+			},
+			{
+				Path:     ".next_withdrawal_validator_index",
+				Expected: pbState.NextWithdrawalValidatorIndex,
+			},
+			// Test Electra-specific fields
+			{
+				Path:     ".deposit_requests_start_index",
+				Expected: pbState.DepositRequestsStartIndex,
+			},
+			{
+				Path:     ".deposit_balance_to_consume",
+				Expected: pbState.DepositBalanceToConsume,
+			},
+			{
+				Path:     ".exit_balance_to_consume",
+				Expected: pbState.ExitBalanceToConsume,
+			},
+			{
+				Path:     ".earliest_exit_epoch",
+				Expected: pbState.EarliestExitEpoch,
+			},
+			{
+				Path:     ".consolidation_balance_to_consume",
+				Expected: pbState.ConsolidationBalanceToConsume,
+			},
+			{
+				Path:     ".earliest_consolidation_epoch",
+				Expected: pbState.EarliestConsolidationEpoch,
+			},
+			// Test historical summaries - entire list
+			{
+				Path:     ".historical_summaries",
+				Expected: pbState.HistoricalSummaries,
+			},
+			// Test pending deposits - entire list
+			{
+				Path:     ".pending_deposits",
+				Expected: pbState.PendingDeposits,
+			},
+			// Test pending partial withdrawals - entire list
+			{
+				Path:     ".pending_partial_withdrawals",
+				Expected: pbState.PendingPartialWithdrawals,
+			},
+			// Test pending consolidations - entire list
+			{
+				Path:     ".pending_consolidations",
+				Expected: pbState.PendingConsolidations,
+			},
+		},
+	}
+}
+
+// generateUniqueBytes generates a byte slice of given length with unique sequential values
+// starting from the given offset. It wraps around at 256.
+func generateUniqueBytes(length int, offset int) []byte {
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = byte((offset + i) % 256)
+	}
+	return result
 }
